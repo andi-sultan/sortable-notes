4e316a6b18e97b3604ec96560703a77dda1d336b
Andi Sultan <andisultan317@gmail.com>
2022-09-14 16:22:36 +0800

    Revert "Show latest notes without label"
    This reverts commit ddb81561641a59f0fb8b61cb8964e79ee653675a.

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -32,7 +32,7 @@ class NoteController extends Controller
     public function getNotes(Request $request)
     {
         if ($request->ajax()) {
-            $note = Note::where('user_id', 2)->doesntHave('noteLabel')->latest();
+            $note = Note::query()->where('user_id', 2)->doesntHave('noteLabel');
 
             return DataTables::eloquent($note)
                 ->addIndexColumn()
