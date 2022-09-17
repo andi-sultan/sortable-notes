6279ca0e8a25092f3ccfab06606c8cc09cc52096
Andi Sultan <andisultan317@gmail.com>
2022-09-17 18:09:26 +0800

    Add softdelete on noteLabel

--- app/Models/NoteLabel.php
+++ app/Models/NoteLabel.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use App\Models\Note;
 use App\Models\Label;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class NoteLabel extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $fillable = ['note_id', 'label_id', 'position'];
 
     public function note()
