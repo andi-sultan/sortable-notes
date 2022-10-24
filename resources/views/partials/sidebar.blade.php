46a1b5b967d34709894aaec48a7325d635ce55dc
Andi Sultan <andisultan317@gmail.com>
2022-10-24 13:29:08 +0800

    Revert "Fix sidebar can't scroll"
    This reverts commit a3ca3e7c7c07836e13ea053a9143c3d43b5c1159.

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4 overflow-auto">
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
         <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
