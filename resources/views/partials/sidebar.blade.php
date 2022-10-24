3b21ee89eff591e9bad467c70a215bb1f8c1289d
Andi Sultan <andisultan317@gmail.com>
2022-10-24 13:17:18 +0800

    Fix sidebar can't scroll

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4 overflow-auto">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
         <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
